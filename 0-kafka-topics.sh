0c09316cdc9bb745e7b9f88b23a1e67a628f22b2
hebertcisco <tecnobertinfo@gmail.com>
2021-07-04 23:16:05 -0300

    first commit

---
+++ 0-kafka-topics.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+kafka-topics
+
+kafka-topics --zookeeper 127.0.0.1:2181 --list
+
+kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --create
+
+kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --create --partitions 3
+
+kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --create --partitions 3 --replication-factor 2
+
+kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --create --partitions 3 --replication-factor 1
+
+kafka-topics --zookeeper 127.0.0.1:2181 --list
+
+kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --describe
+
+kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --delete
